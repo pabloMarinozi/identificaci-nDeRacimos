b510443f73a27382e6b630e01c311c0bc07a0b24
Valenb1610 <112518528+Valenb1610@users.noreply.github.com>
2022-11-10 10:53:54 -0300

    Actualizacion de tiangulate.py

--- Triangulacion/triangulate.py
+++ Triangulacion/triangulate.py
@@ -1,12 +1,12 @@
 import subprocess
 import os
-
+import cv2
 
 
 # Directorio que contiene a todos los cuarteles a procesar
-#input_dir = "../2022-02-17_Zuccardi_Piedra-Infinita_Volumetria/data_sources/fallidas_reintento"
-input_dir = "/home/pablo/DHARMA/berry_tracking/output/VID_20220217_101324/mod4"
-image_dir = "/home/pablo/DHARMA/berry_tracking/output/misnubes"
+# input_dir = "../2022-02-17_Zuccardi_Piedra-Infinita_Volumetria/data_sources/fallidas_reintento"
+input_dir = "/home/valen/input_bundles_from_detections/VID_20220217_153655/mod4"
+image_dir = "/home/valen/input_bundles_from_detections/VID_20220217_153655"
 # Extesión de las imágenes a procesar (sin el punto)
 ext_img = "png"
 # Calibración usada durante la reconstrucción 3D y estimación de volúmenes
@@ -15,13 +15,18 @@ calib = 2
 val_dist = 2
 # Nombre del directorio de salida para la info de los volúmenes de cada cuartel
 path_vol = "volumes"
-
+# imagen
+img = cv2.imread(image_dir, cv2.IMREAD_UNCHANGED)
+alto = img.shape[0]
+ancho = img.shape[1]
 print("\n\n")
 print("#"*80)
 print("\n>>> COMPUTE VOLUMES FROM BUNDLES")
 
-calib_yaml = "camaraMaurosalentein.yaml"
-# calib_yaml = "camaraMaurosalenteinVertical.yaml"
+if alto < ancho:
+    calib_yaml = "camaraMaurosalentein.yaml"
+else:
+    calib_yaml = "camaraMaurosalenteinVertical.yaml"
 
 path_temp = os.path.join(input_dir, "volumes/")
 if not os.path.exists(path_temp):
